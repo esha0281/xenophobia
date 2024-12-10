793f9ded1fe48a51363489e163146e6b6334f1a7
Darmin T. <61888777+CoderTea-1@users.noreply.github.com>
2024-12-10 10:10:22 -0800

    Update ImplicitBiasTestBlocks.js

--- src/constants/ImplicitBiasTestBlocks.js
+++ src/constants/ImplicitBiasTestBlocks.js
@@ -2,32 +2,32 @@ import DISPLAY_TYPE from '../constants/TargetCategoryDisplayType';
 
 const IMPLICIT_BIAS_TEST_BLOCKS = [
   {
-    numTrials: 2,
+    numTrials: 10,
     displayType: DISPLAY_TYPE.INCOMPATIBLE_TARGETS_ONLY
   },
   {
-    numTrials: 2,
+    numTrials: 10,
     displayType: DISPLAY_TYPE.CATEGORIES_ONLY
   },
   {
-    numTrials: 2,
+    numTrials: 10,
     displayType: DISPLAY_TYPE.INCOMPATIBLE_ALL
   },
   {
-    numTrials: 4,
+    numTrials: 20,
     displayType: DISPLAY_TYPE.INCOMPATIBLE_ALL,
     critical: true
   },
   {
-    numTrials: 2,
+    numTrials: 10,
     displayType: DISPLAY_TYPE.COMPATIBLE_TARGETS_ONLY
   },
   {
-    numTrials: 2,
+    numTrials: 10,
     displayType: DISPLAY_TYPE.COMPATIBLE_ALL
   },
   {
-    numTrials: 4,
+    numTrials: 20,
     displayType: DISPLAY_TYPE.COMPATIBLE_ALL,
     critical: true
   }
